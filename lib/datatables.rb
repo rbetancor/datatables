b86145853fda2b5d1b7c486f628cee4e2a196bfc
Raul Alexis Betancor Santana <rbetancor@gmail.com>
2014-03-05 22:01:23 +0000

    Upgrade gemspec, also code to work with Rails 4

--- lib/datatables.rb
+++ lib/datatables.rb
@@ -1,5 +1,6 @@
 module Datatables
-  require 'datatables/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  require 'datatables/activerecord_methods' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  require 'datatables/datatable_helpers' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  require 'datatables/version'
+  require 'datatables/engine' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+  require 'datatables/activerecord_methods' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+  require 'datatables/datatable_helpers' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 end
---
+++ lib/datatables/version.rb
@@ -0,0 +1,3 @@
+module Datatables
+  VERSION = "1.2.0"
+end
