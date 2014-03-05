539db1d864f44181f87f026cfd641b8b993576be
Raul Alexis Betancor Santana <rbetancor@gmail.com>
2014-03-05 22:09:27 +0000

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match 'rdtable/:model/:columns' => 'datatables#dataset', :as => 'datatable'
-  match 'rdtable/:model/:columns/:scope' => 'datatables#dataset', :as => 'datatable'
-  match 'rdtable/:model/:columns/:scope/:id' => 'datatables#dataset', :as => 'datatable'
+  get 'rdtable/:model/:columns' => 'datatables#dataset'
+  get 'rdtable/:model/:columns/:scope' => 'datatables#dataset'
+  get 'rdtable/:model/:columns/:scope/:id' => 'datatables#dataset'
 end
